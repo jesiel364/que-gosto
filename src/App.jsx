c472fae82592d2307d6f4f8e84b34b1029af427f
jesiel364 <jesiel364@gmail.com>
2023-07-28 18:38:16 -0400

    esqueleto do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,144 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import controls from "../src/assets/icons/controls.svg";
+import fire from "../src/assets/icons/fire.svg";
+import hot from "../src/assets/icons/hot.svg";
+import magnify from "../src/assets/icons/magnify.svg";
+import pimenta from "../src/assets/icons/pimenta.svg";
+import pizza from "../src/assets/icons/pizza.svg";
+import saveReg from "../src/assets/icons/bookmark-regular.svg";
+import saveSolid from "../src/assets/icons/save-solid.svg";
+import sine from "../src/assets/icons/sino.svg";
+import sorvete from "../src/assets/icons/sorvete.svg";
+import user from "../src/assets/icons/user.svg";
+import clock from "../src/assets/icons/clock-regular.svg";
+import star from "../src/assets/icons/star-solid.svg";
+import plus from "../src/assets/icons/plus.svg";
+import house from "../src/assets/icons/house.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Toolbar() {
+  return (
+    <div id="toolbar">
+      <img src={user} />
+      <img src={sine} />
+    </div>
+  );
+}
+
+function Wellcome() {
+  return (
+    <div id="well">
+      <p>Hello. Teresa!</p>
+      <p>Make your own food, stay at home</p>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div id="search">
+      <input />
+      <img src={magnify} id="magnify" />
+      <label>Search any recipe</label>
+      <img src={controls} id="controls" />
+    </div>
+  );
+}
+
+function Carrossel() {
+  const items = [
+    {
+      id: 1,
+      title: "Popular",
+      icon: fire,
+    },
+    {
+      id: 2,
+      title: "Western",
+      icon: pizza,
+    },
+    {
+      id: 3,
+      title: "Drinks",
+      icon: hot,
+    },
+    {
+      id: 4,
+      title: "Desserve",
+      icon: sorvete,
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="carrossel-menu">
+      {items.map((item) => (
+        <div key={item.id} id="carrossel-item">
+          <img src={item.icon} />
+          <p id="title">{item.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Card(props){
+ return (
+  <div id="card">
+
+    <img src={props.img} />
+    <div id="time">
+      <img src={clock} />
+    </div>
+
+    <div id="rate">
+      <img src={star} />
+      <p>4.8</p>
+    </div>
+  </div>
+ )
+}
+
+function Popular(){
+  return(
+    <div id="popular">
+      <p>Popular Recipes</p>
+
+      <Card img='https://cdn.panelinha.com.br/receita/1588270905274-39_Panelinha_12_02_200635.jpg' />
+      <Card img='https://cdn.panelinha.com.br/receita/1588270905274-39_Panelinha_12_02_200635.jpg' />
+      <Card img='https://cdn.panelinha.com.br/receita/1588270905274-39_Panelinha_12_02_200635.jpg' />
+    </div>
   )
 }
 
-export default App
+function Navigation(){
+  return (
+    <div id="navigation">
+      <div id="home">
+        <img src={house} />
+        <p>Home</p>
+      </div>
+
+      <img id='plus' src={plus} />
+      
+      <img id="save" src={saveSolid} />
+
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <div id="container">
+        <Toolbar />
+        <Wellcome />
+        <Search />
+        <Carrossel />
+        <Popular />
+        <Navigation />
+      </div>
+    </>
+  );
+}
+
+export default App;
